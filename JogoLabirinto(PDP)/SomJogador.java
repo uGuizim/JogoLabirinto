ee1541b8cbc173f89fe12bbaba834cab7d57614b
uGuizim <128945151+uGuizim@users.noreply.github.com>
2023-12-04 11:55:43 -0300

    .

--- JogoLabirinto(PDP)/SomJogador.java
+++ JogoLabirinto(PDP)/SomJogador.java
@@ -2,6 +2,6 @@ import greenfoot.*;
 
 public class SomJogador implements ComponenteSom {
     public void reproduzirSom() {
-        Greenfoot.playSound("footstep.mp3"); // Substitua "som_passos.mp3" pelo nome do seu arquivo de som
+        Greenfoot.playSound("footstep.mp3"); 
     }
 }
