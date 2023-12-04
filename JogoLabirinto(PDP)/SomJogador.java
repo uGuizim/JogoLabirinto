84e4b8ee1469f80a3c85a90c59cbded14e441cb3
luisdk2000 <129339959+luisdk2000@users.noreply.github.com>
2023-12-04 16:23:14 -0300

    .

--- JogoLabirinto(PDP)/SomJogador.java
+++ JogoLabirinto(PDP)/SomJogador.java
@@ -1,7 +1,20 @@
 import greenfoot.*;  
 
 public class SomJogador implements ComponenteSom {
+    private GreenfootSound somPassos;
+
+    public SomJogador() {
+        somPassos = new GreenfootSound("footstep.mp3");
+        somPassos.setVolume(50); // Define o volume para 50%
+    }
+
     public void reproduzirSom() {
-        Greenfoot.playSound("footstep.mp3"); 
+        if (!somPassos.isPlaying()) {
+            somPassos.play();
+        }
+    }
+
+    public void pararSom() {
+        somPassos.stop();
     }
 }
